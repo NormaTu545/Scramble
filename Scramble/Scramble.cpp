46afd834ce4b3c9558749e6e75fd4b4a89852aee
NormaTu545 <NormaTu545@gmail.com>
2016-08-20 18:26:27 -0700

    [Initial Commit]
    Set up SFML Project in Visual Studio.

--- Scramble/Scramble.cpp
+++ Scramble/Scramble.cpp
@@ -2,10 +2,36 @@
 //
 
 #include "stdafx.h"
+#include <SFML/Graphics.hpp>
+
+#define WINDOW_WIDTH 1000
+#define WINDOW_HEIGHT 500
+
+int main() {
+
+	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Scramble_Game");
+
+	//Limit the framerate to 60 frames per second
+	window.setFramerateLimit(60);
 
 
-int main()
-{
-    return 0;
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+    return EXIT_SUCCESS;
 }
 
